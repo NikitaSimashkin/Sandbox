7bc95699b8d645ed57adc695af3e45389d407411
NikitaSimashkin <91666467+NikitaSimashkin@users.noreply.github.com>
2024-12-30 02:41:28 +0100

    set java to 17 (#4)

--- gradle-plugins/src/main/java/ru/kram/sandbox/build/Config.kt
+++ gradle-plugins/src/main/java/ru/kram/sandbox/build/Config.kt
@@ -1,7 +1,11 @@
 package ru.kram.sandbox.build
 
+import org.gradle.api.JavaVersion
+
 object Config {
     const val minSdkVersion = 28
     const val targetSdkVersion = 35
     const val compileSdkVersion = 35
+    val java = JavaVersion.VERSION_17
+    val javaString = "17"
 }
